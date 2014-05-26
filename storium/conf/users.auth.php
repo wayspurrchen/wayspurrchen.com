bd862df971b898cd0f2b9d320e71e13985733d1e
Way Spurr-Chen <wayspurrchen@gmail.com>
2014-05-25 21:48:01 -0500

    Add Dokuwiki

---
+++ storium/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+ways:$1$bs0BpiEB$ZEfU1U2a9dDnnivZHX6Z90:Way Spurr-Chen:wayspurrchen@gmail.com:admin,user
