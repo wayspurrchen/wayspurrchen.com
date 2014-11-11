89bd6a43d6e8abe71f759b4af9961221cbccfff8
Way Spurr-Chen <wayspurrchen@gmail.com>
2014-11-10 18:41:56 -0600

    Tweak file order

--- app.js
+++ app.js
@@ -3,17 +3,16 @@ var app = express();
 var request = require('request');
 var queryString = require('query-string');
 var path = require('path');
+app.listen(process.env.WAYSPURRCHEN_COM_PORT || 80);
 
 // API
 var api = require('./server/api');
-app.use('/api', api);
-var server = app.listen(process.env.WAYSPURRCHEN_COM_PORT || 80);
-
 var allowedOrigins = [];
 
 // Set static assets config here
 var publicDir = path.resolve(__dirname, 'public');
 app.use(express.static(publicDir));
+app.use('/api', api);
 
 // Check if the file they're requesting exists. If so, let them
 // continue to default behaviors, otherwise send them back
